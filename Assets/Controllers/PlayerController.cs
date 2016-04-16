56797431b2dff2077a74d9184ea95fecfc186336
alexandercstephens <alexandercstephens@gmail.com>
2016-04-16 09:56:01 -0500

    keep player inside boundary box

--- Assets/Controllers/PlayerController.cs
+++ Assets/Controllers/PlayerController.cs
@@ -2,8 +2,10 @@
 
 public class PlayerController : MonoBehaviour {
     public float movementSpeed = 1f;
+    public BoxCollider boundaryBox;
 
     private CameraController cameraController;
+    private BoxCollider playerBox;
 
     private bool isTopDown;
     private Vector3 movementVector;
@@ -11,6 +13,7 @@ public class PlayerController : MonoBehaviour {
     void Awake ()
     {
         cameraController = GameObject.Find("CameraManager").GetComponent<CameraController>();
+        playerBox = GetComponent<BoxCollider>();
     }
 
     // Use this for initialization
@@ -71,7 +74,7 @@ public class PlayerController : MonoBehaviour {
         isTopDown = itd;
     }
 
-    //checks for movement input and moves character
+    //checks for movement input and moves player
     private void Move ()
     {
         float horizontalMovement = Input.GetAxis("Horizontal");
@@ -86,5 +89,33 @@ public class PlayerController : MonoBehaviour {
             movementVector = new Vector3(0f, verticalMovement, horizontalMovement);
         }
         this.transform.Translate(movementSpeed * Time.deltaTime * movementVector);
+
+        //keep player inside bounds -gotta be a better way to do this
+        if (playerBox.bounds.min.x < boundaryBox.bounds.min.x)
+        {
+            this.transform.Translate(new Vector3(boundaryBox.bounds.min.x - playerBox.bounds.min.x, 0f, 0f));
+        }
+        else if (playerBox.bounds.max.x > boundaryBox.bounds.max.x)
+        {
+            this.transform.Translate(new Vector3(boundaryBox.bounds.max.x - playerBox.bounds.max.x, 0f, 0f));
+        }
+
+        if (playerBox.bounds.min.y < boundaryBox.bounds.min.y)
+        {
+            this.transform.Translate(new Vector3(0f, boundaryBox.bounds.min.y - playerBox.bounds.min.y, 0f));
+        }
+        else if (playerBox.bounds.max.y > boundaryBox.bounds.max.y)
+        {
+            this.transform.Translate(new Vector3(0f, boundaryBox.bounds.max.y - playerBox.bounds.max.y, 0f));
+        }
+
+        if (playerBox.bounds.min.z < boundaryBox.bounds.min.z)
+        {
+            this.transform.Translate(new Vector3(0f, 0f, boundaryBox.bounds.min.z - playerBox.bounds.min.z));
+        }
+        else if (playerBox.bounds.max.z > boundaryBox.bounds.max.z)
+        {
+            this.transform.Translate(new Vector3(0f, 0f, boundaryBox.bounds.max.z - playerBox.bounds.max.z));
+        }
     }
 }
