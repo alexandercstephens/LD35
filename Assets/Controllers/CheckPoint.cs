722bf8d63f5205628f43e7f6356a8bcf5c53a2de
JayLenahan <lenahanjay@gmail.com>
2016-04-16 18:59:18 -0500

    checkp

--- Assets/Controllers/CheckPoint.cs
+++ Assets/Controllers/CheckPoint.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CheckPoint : MonoBehaviour {
+public class CheckPoint : MonoBehaviour
+{
+    protected string Check;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
 }
