ebbd36d8455d103f4277e8efb6514b7b2cabfbbe
alexandercstephens <alexandercstephens@gmail.com>
2016-04-16 12:29:11 -0500

    added noise material and script

---
+++ Assets/Controllers/Noise.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Noise : MonoBehaviour {
+    private Renderer r;
+
+    void Awake ()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+	void Update () {
+        r.material.SetTextureOffset("_MainTex", new Vector2(Random.value, Random.value));
+    }
+}
