0eb1d10accf1a2702fe8d89aac715cbcada0a8c6
Alexander Stephens <alexandercstephens@gmail.com>
2016-04-17 08:28:17 -0500

    added enemy explosion on death

--- Assets/Controllers/SpearheadController.cs
+++ Assets/Controllers/SpearheadController.cs
@@ -1,43 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpearheadController : MonoBehaviour {
-    private bool isActive = false;
-    private float timeTurnedActive; //needed to prevent isActive from immediately switching off
-    private Vector3 originalPosition;
-    private Vector3 originalScale;
+public class SpearheadController : MonoBehaviour
+{
+	private CameraController cameraController;
+
+	private bool isActive = false;
+	private float timeTurnedActive;
+	//needed to prevent isActive from immediately switching off
+	private Vector3 originalPosition;
+	private Vector3 originalScale;
 
 	// Use this for initialization
 	public void Spear ()
-    {
-        transform.localPosition = new Vector3(0f, 0f, 10f + 0.5f);
-        transform.localScale = new Vector3(1f, 1f, 20f);
+	{
+		cameraController = GameObject.Find ("CameraManager").GetComponent<CameraController> ();
 
-        isActive = true;
-        timeTurnedActive = Time.time;
-    }
+		transform.localPosition = new Vector3 (0f, 0f, 10f + 0.5f);
+		transform.localScale = new Vector3 (1f, 1f, 20f);
 
-    void Awake()
-    {
-        originalPosition = transform.localPosition;
-        originalScale = transform.localScale;
-    }
+		isActive = true;
+		timeTurnedActive = Time.time;
+	}
 
-    void Update()
-    {
-        transform.localPosition = Vector3.Lerp(transform.localPosition, originalPosition, 0.1f);
-        transform.localScale = Vector3.Lerp(transform.localScale, originalScale, 0.1f);
-        if (transform.localPosition.x - originalPosition.x < 0.01f && isActive && Time.time - timeTurnedActive > 0.1f)
-        {
-            isActive = false;
-        }
-    }
+	void Awake ()
+	{
+		originalPosition = transform.localPosition;
+		originalScale = transform.localScale;
+	}
 
-    void OnTriggerEnter(Collider collider)
-    {
-        if (isActive && collider.tag == "HurtsPlayer")
-        {
-            Destroy(collider.gameObject);//TODO make enemy killing more robust
-        }
-    }
+	void Update ()
+	{
+		transform.localPosition = Vector3.Lerp (transform.localPosition, originalPosition, 0.1f);
+		transform.localScale = Vector3.Lerp (transform.localScale, originalScale, 0.1f);
+		if (transform.localPosition.x - originalPosition.x < 0.01f && isActive && Time.time - timeTurnedActive > 0.1f) {
+			isActive = false;
+		}
+	}
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (isActive && collider.tag == "HurtsPlayer") {
+			Destroy (collider.gameObject);//TODO make enemy killing more robust
+			var fracturedObject = collider.gameObject.GetComponent<FracturedObject> ();
+			if (fracturedObject != null) {
+				fracturedObject.Explode (collider.gameObject.transform.position, 3f + Random.value * 5f);
+				cameraController.addShake (0.44444444444f); //the length of one beat of the song
+			}
+		}
+	}
 }
