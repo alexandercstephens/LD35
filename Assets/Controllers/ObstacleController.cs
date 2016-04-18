12923d7a2ed42e4cf85f6100873f6208147404b1
RickDork <darkcyborg1@gmail.com>
2016-04-18 01:40:17 -0500

    Checkpoints

--- Assets/Controllers/ObstacleController.cs
+++ Assets/Controllers/ObstacleController.cs
@@ -13,7 +13,8 @@ public class ObstacleController : MonoBehaviour {
 	private Vector3 StartPos;
 	private float StartTime { get; set; }
 	private float StartSceneZ = 0.0f;
-
+	public float BackAndForthPause = 0.0f;
+	private float BackAndForthPauseStart = 0.0f;
     public float RotateXSpeed = 0.0f;
     public float RotateYSpeed = 0.0f;
 	public float RotateZSpeed = 0.0f;
@@ -33,17 +34,34 @@ public class ObstacleController : MonoBehaviour {
 				if (!getStartPos) {
 					getStartPos = true;
 					StartPos = this.transform.position;
-					StartSceneZ = GameObject.Find ("MovingScene").transform.position.z;
+					StartSceneZ = GameObject.FindGameObjectWithTag( "Checkpoint" ).transform.position.z;
 					StartTime = Time.time;
+					BackAndForthPauseStart = 0.0f;
 				}
 
 				Vector3 newPos = StartPos + DestinationOffset;
-				newPos.z += (GameObject.Find ("MovingScene").transform.position.z - StartSceneZ);
-				this.transform.position = Vector3.Lerp (StartPos, newPos, (Time.time - StartTime) / time);
+				newPos.z += (GameObject.FindGameObjectWithTag( "Checkpoint" ).transform.position.z - StartSceneZ);
+				Debug.Log (newPos.y);
+				if (time > 0.0f)
+					this.transform.position = Vector3.Lerp (StartPos, newPos, (Time.time - StartTime) / time);
+				else
+					this.transform.position = newPos;
 
-				if (BackAndForth) {
-					getStartPos = false;
+				if (BackAndForth && Time.time - StartTime > time ) {
+					bool r = false;
 
+					if (BackAndForthPause > 0.0f) {
+						if (BackAndForthPauseStart < 1.0f)
+							BackAndForthPauseStart = Time.time;
+						else if (Time.time - BackAndForthPauseStart > BackAndForthPause)
+							r = true;
+					} else
+						r = true;
+					
+					if (r) {	
+						getStartPos = false;
+						DestinationOffset = -DestinationOffset;
+					}
 				}
 
 			}
